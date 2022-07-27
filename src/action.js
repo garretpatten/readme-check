36449f956b1583aa259c651c8ba76ecfc32a69a7
Garret Patten <garret.patten@proton.me>
2022-07-27 11:08:46 -0400

    Add validation for top line of README

--- src/action.js
+++ src/action.js
@@ -5,19 +5,52 @@ const github = require('@actions/github');
 /* Node Dependencies */
 const fs = require('fs');
 
+/* I/O */
 const INPUT_README_PATH = 'readmePath';
 const OUTPUT_TIMESTAMP = 'time';
 
-console.log('Entering try block');
+const repoName = github.event.repository.name;
+
+/* Validates the title of the README
+ * which should be an image title or
+ * a top level markdown heading whose
+ * title matches the repository name
+ */
+function validateTopLine(topLine) {
+	const topLineSplit = topLine.split(' ');
+
+	// Check for image title
+	if (topLineSplit.length === 1 && topLineSplit[0].substring(0, 2) !== '[!') {
+		return;
+	}
+
+	// Check for markdown heading
+	if (topLineSplit[0] === '#'
+		|| topLineSplit[1].toUpperCase() != repoName.toUpperCase()) {
+			return;
+	}
+
+	console.log(topLine);
+	console.log(topLineSplit);
+	console.log(repoName);
+	core.setFailed('The top line of the README should contain a descriptive image or a top level markdown heading that matches the repository name');
+}
 
 try {
 	const readmePath = core.getInput(INPUT_README_PATH);
 
 	const readmeMetadata = fs.readFileSync(readmePath, 'utf8');
-	const fileLines = readmeMetadata.split('\n');
+	const readmeLines = readmeMetadata.split('\n');
+
+	if (readmeLines.length == 0) {
+		core.setFailed('The README file is empty.');
+	}
+
+	const topLine = readmeLines[0];
+	validateTopLine(topLine);
 
 	// TODO: Process README content
-	for (let fileLine of fileLines) {
+	for (let fileLine of readmeLines) {
 		console.log('LINE START');
 		console.log(fileLine);
 		console.log('LINE END');
